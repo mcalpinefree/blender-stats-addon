304c0f0e9c2a9291fd6d8ed5b56f932241f4ac42
Kurt McAlpine <kurt@linux.com>
2020-09-08 20:00:42 +1200

    Demonstrate pushing stats

--- operators/buttons.py
+++ operators/buttons.py
@@ -2,6 +2,7 @@
 import bpy
 import json
 import requests
+from datetime import datetime
 from ..core.stats import SnapshotStats
 from ..core.stats_queue import StatsQueue
 from ..core.auth import Authenticator
@@ -17,6 +18,33 @@ class CollectStats(bpy.types.Operator):
     def execute(self, context):
         stats = SnapshotStats()
         stats.collect()
+
+        # push counts
+        t = datetime.now()
+        s = t.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        time = s[:-3] + "Z"
+        preferences = context.preferences
+        addon_prefs = preferences.addons[Preferences.bl_idname].preferences
+        cognito = json.loads(addon_prefs.user_token)
+        r = requests.get("https://api.blender-stats.staging.mcalpinefree.io/user",
+                         headers={"Authorization": "Bearer " + cognito['id_token']})
+        response = json.loads(r.content)
+        print(response)
+        # this is a hack, we need to choose the project the stats are associated with
+        project_id = response["project_ids"][0]
+        for key, value in stats.stats.items():
+            if key == "blender_version":
+                continue
+            print("project_id {}, time {}, count {}, count_item {}".format(
+                project_id, time, value, key))
+            r = requests.post("https://api.blender-stats.staging.mcalpinefree.io/stat/count",
+                              headers={"Authorization": "Bearer " +
+                                       cognito['id_token']},
+                              json={"project_id": project_id,
+                                    "time": time,
+                                    "count": value,
+                                    "count_item": key})
+
         StatsQueue.add(stats)
         return {'FINISHED'}
 
@@ -29,6 +57,7 @@ class StartLogin(bpy.types.Operator):
     def execute(self, context):
         bpy.ops.object.login_manager('INVOKE_DEFAULT')
         return {'FINISHED'}
+
 
 class Logout(bpy.types.Operator):
     '''Logout'''
