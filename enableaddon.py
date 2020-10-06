182be2e7edc3dbe068ca3f5744323956330d174c
Kurt McAlpine <kurt@linux.com>
2020-10-06 20:44:01 +1300

    Improve install script

--- enableaddon.py
+++ enableaddon.py
@@ -1,7 +1,19 @@
+import shutil
 import bpy
 import pathlib
+import os
+from os import path
 
 current_dir = pathlib.Path(__file__).parent.absolute()
+
+user_path = bpy.utils.resource_path('USER')
+addons_path = path.join(user_path, "scripts/addons")
+blender_stats_installed_path = path.join(addons_path, "blender-stats-addon")
+
+if path.exists(blender_stats_installed_path):
+    shutil.rmtree(blender_stats_installed_path)
+
+shutil.copytree(current_dir, blender_stats_installed_path)
 
 bpy.ops.preferences.addon_install(
     filepath='{}/__init__.py'.format(current_dir))
