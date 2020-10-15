90e9e58b3c2a3dd09a31fcafab6b19de168dcdee
joshofreeness <joshofreeness@gmail.com>
2020-10-15 21:58:19 +1300

    wip

--- panels/__init__.py
+++ panels/__init__.py
@@ -1,11 +1,15 @@
 
 import bpy
-from .panel import BlenderStatsMainPanel, BlenderStatsProjectPanel
+from .panel import BlenderStatsMainPanel, BlenderStatsProjectPanel, MyShortAddonProperties
 
 def register():
     bpy.utils.register_class(BlenderStatsMainPanel)
     bpy.utils.register_class(BlenderStatsProjectPanel)
+    bpy.utils.register_class(MyShortAddonProperties)
+    bpy.types.Scene.custom_settings = bpy.props.PointerProperty(type=MyShortAddonProperties)
 
 def unregister():
+    del bpy.types.Scene.custom_settings
     bpy.utils.unregister_class(BlenderStatsMainPanel)
     bpy.utils.unregister_class(BlenderStatsProjectPanel)
+    bpy.utils.unregister_class(MyShortAddonProperties)
--- panels/panel.py
+++ panels/panel.py
@@ -38,14 +38,67 @@ class BlenderStatsMainPanel(BlenderStatsBasePanel, bpy.types.Panel):
             self.layout.label(text="Something went wrong, unknown login state")
 
 
+def execute_operator(self, context):
+    eval('bpy.ops.' + self.primitive + '()')
+
+class MyShortAddonProperties(bpy.types.PropertyGroup):
+    mode_options = [
+        ("mesh.primitive_plane_add", "Plane", '', 'MESH_PLANE', 0),
+        ("mesh.primitive_cube_add", "Cube", '', 'MESH_CUBE', 1),
+        ("mesh.primitive_circle_add", "Circle", '', 'MESH_CIRCLE', 2),
+        ("mesh.primitive_uv_sphere_add", "UV Sphere", '', 'MESH_UVSPHERE', 3),
+        ("mesh.primitive_ico_sphere_add", "Ico Sphere", '', 'MESH_ICOSPHERE', 4),
+        ("mesh.primitive_cylinder_add", "Cylinder", '', 'MESH_CYLINDER', 5),
+        ("mesh.primitive_cone_add", "Cone", '', 'MESH_CONE', 6),
+        ("mesh.primitive_torus_add", "Torus", '', 'MESH_TORUS', 7)
+    ]
+
+    # primitive: bpy.props.EnumProperty(
+    #     items=mode_options,
+    #     description="offers....",
+    #     default="mesh.primitive_plane_add",
+    #     update=execute_operator
+    # )
+
+    
+
 class BlenderStatsProjectPanel(BlenderStatsBasePanel, bpy.types.Panel):
     bl_idname = "OBJECT_PT_blenderstatsprojects"
     bl_label = "Projects"
+
+    primitive:  bpy.props.EnumProperty(
+        name="My Settings",
+        description="Custom Settings",
+        items=[
+            ('S1', "Setting One", "", 0),
+            ('S2', "Setting Two", "", 1),
+            ('S3', "Setting Three", "", 2),
+        ],
+        default='S1'
+    )
+
+    # my_short_addon = bpy.props.PointerProperty(type=MyShortAddonProperties)
+
+    # fixed_items = bpy.props.EnumProperty(items= [('0', 'A', 'The zeroth item'),    
+    #                                             ('1', 'B', 'The first item'),    
+    #                                             ('2', 'C', 'The second item'),    
+    #                                             ('3', 'D', 'The third item')],
+    #                                             name = "fixed list")
 
     def draw(self, context):
         preferences = context.preferences
         addon_prefs = preferences.addons[Preferences.bl_idname].preferences
         user = User.get_local_user(addon_prefs)
+
+        # self.layout.operator("object.select_all", text="Select/Deselect All").action = 'TOGGLE'
+        # self.layout.operator("object.select_all", text="Inverse").action = 'INVERT'
+        # self.layout.operator("object.select_random", text="Random")
+        settings = context.scene.custom_settings
+        print(settings)
+        print(self.my_short_addon)
+        self.layout.prop(self, "primitive")
+
+        # self.layout.menu("INFO_MT_add", text="Add")
 
         for project in user.projects:
             self.layout.label(text=project["name"])
